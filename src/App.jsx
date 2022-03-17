3cc6b18af71f2112cf85c686429267dd0be71d75
shumphries11 <santitahumphries@gmail.com>
2022-03-17 16:09:24 -0500

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -25,28 +25,28 @@ const App = () => {
     return (
         <div>
 
-            <form className="border border-dark col-sm-4 p-4 m-3 bg-color" onSubmit={handleClickButton}>
+            <form className="border border-dark col-md-4 p-4 m-3 bg-color" onSubmit={handleClickButton}>
 
-                <div className="form-group">
+                <div className="form-group text-center">
                      <textarea
-                     className="p-3"
+                     className="p-3 "
                      name="newMessage" value={newMessages} onChange={handleMessageChange} placeholder="Say Something" >
                 </textarea> 
                 </div>
                
-                <div className="form-group d-flex justify-content-center">
+                <div className="form-group text-center">
                     <button>Post Chirp</button>
                 </div>
 
             </form>
 
 
-            <h1 className="border border-dark col-md-8 p-3 m-3"> <Chirps message='Hello' /></h1>
-            <h1 className="border border-dark col-md-8 p-3 m-3"> <Chirps message='Good Morning' /></h1>
-            <h1 className="border border-dark col-md-8 p-3 m-3"> <Chirps message='Hru?' /></h1>
+            <h3 className="border border-dark col-md-3 p-3 m-3"> <Chirps message='Hello' /></h3>
+            <h3 className="border border-dark col-md-3 p-3 m-3"> <Chirps message='Good Morning' /></h3>
+            <h3 className="border border-dark col-md-3 p-3 m-3"> <Chirps message='Hru?' /></h3>
             {
                 post.map(text => {
-                    return <h1 className="border border-dark col-md-8 p-3 m-3">{text}</h1>
+                    return <h3 className="border border-dark col-md-3 p-3 m-3">{text}</h3>
                 })
             }
 
